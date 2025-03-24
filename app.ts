4050f615ebba1a2b52453effb6faaf33cd42eb69
Danton Lucas <danyonlucasrr@gmail.com>
2025-03-24 19:53:58 -0300

    first commit

--- app.ts
+++
@@ -1,6 +0,0 @@
-let nome: string;
-let idade: number;
-
-nome= "Danton Lucas";
-idade= 24
-console.log(`nome: ${nome}, idade: ${idade}` )
--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let nome;
-let idade;
-nome = "Danton";
-idade = 24;
-console.log(`nome: ${nome}, idade: ${idade}`);
